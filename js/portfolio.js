fe85626f6d39d170a52e29f795e666a276b7a002
Erik Granberg <36399140+erik8036granberg@users.noreply.github.com>
2019-03-21 16:19:07 +0100

    basic portfolio item import ok

--- js/portfolio.js
+++ js/portfolio.js
@@ -6,12 +6,13 @@ window.addEventListener("DOMContentLoaded", init);
 
 function init() {
     console.log("init");
+    getcases()
 }
 
 // - - - - - - - - - - - - - restdb stuff - - - - - - - - - - - - -
 
-function getItems() {
-    console.log("getItems");
+function getcases() {
+    console.log("getcases");
     fetch("https://profweb-4fea.restdb.io/rest/cases?metafields=true&idtolink=true", {
             method: "get",
             headers: {
@@ -24,7 +25,36 @@ function getItems() {
         .then(res => res.json())
         .then(data => {
             console.log(data);
-            // items = data;
-            // sortItems(items);
+            items = data;
+            sortItems(items);
         });
+}
+
+// - - - - - - - - - - - - - sort - - - - - - - - - - - - -
+
+function sortItems(activeitems) {
+    console.log("sortItems");
+    activeitems.sort(function (a, z) {
+        if (a._created < z._created) {
+            return 1;
+        } else {
+            return -1;
+        }
+    });
+    document.querySelector("[data-container]").innerHTML = "";
+    activeitems.forEach(displayCases);
+}
+
+// - - - - - - - - - - - - - display items - - - - - - - - - - - - -
+
+function displayCases(cases) {
+    console.log("displayCases");
+    const template = document.querySelector("[data-template]").content;
+    const clone = template.cloneNode(true);
+    clone.querySelector("[data-id]").dataset.id = cases._id;
+    clone.querySelector("[data-preview]").setAttribute("src", cases.preview_0);
+    clone.querySelector("[data-preview]").setAttribute("alt", cases.customer + " - " + cases.case);
+    clone.querySelector("[data-customer]").textContent = cases.customer;
+    clone.querySelector("[data-case]").textContent = cases.case;
+    document.querySelector("[data-container]").appendChild(clone);
 }
